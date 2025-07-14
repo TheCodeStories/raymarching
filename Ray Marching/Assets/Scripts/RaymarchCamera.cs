75916366d174325d1cf9cf38a00aa9b1ea761968
TheCodeStories <thecodestoriesyt@gmail.com>
2025-07-14 18:51:41 +0200

    Rendering ray directions

---
+++ Ray Marching/Assets/Scripts/RaymarchCamera.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class RaymarchCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Shader _shader;
+
+    public Material _raymarchMaterial
+    {
+        get
+        {
+            if (!_raymarchMat && _shader)
+            {
+                _raymarchMat = new Material(_shader);
+                _raymarchMat.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return _raymarchMat;
+        }
+    }
+    private Material _raymarchMat;
+
+    public Camera _camera
+    {
+        get
+        {
+            if (!_cam)
+            {
+                _cam = GetComponent<Camera>();
+            }
+            return _cam;
+        }
+    }
+
+    private Camera _cam;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (!_raymarchMaterial)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        _raymarchMaterial.SetMatrix("_CamFrustum", CamFrustum(_camera));
+        _raymarchMaterial.SetMatrix("_CamToWorld", _camera.cameraToWorldMatrix);
+
+        RenderTexture.active = destination;
+
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        _raymarchMaterial.SetPass(0);
+        GL.Begin(GL.QUADS);
+
+        //Bottom Left
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f);
+
+        //Bottom Right
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f);
+
+        //Top Right
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f);
+
+        //Top Left
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f);
+
+        GL.End();
+        GL.PopMatrix();
+
+    }
+
+    private Matrix4x4 CamFrustum(Camera cam)
+    {
+        Matrix4x4 frustum = Matrix4x4.identity;
+        float fov = Mathf.Tan((cam.fieldOfView * 0.5f) * Mathf.Deg2Rad);
+
+        Vector3 goUp = Vector3.up * fov;
+        Vector3 goRight = Vector3.right * fov * cam.aspect;
+
+        Vector3 topLeft = (-Vector3.forward - goRight + goUp);
+        Vector3 topRight = (-Vector3.forward + goRight + goUp);
+        Vector3 bottomRight = (-Vector3.forward + goRight - goUp);
+        Vector3 bottomLeft = (-Vector3.forward - goRight - goUp);
+
+        frustum.SetRow(0, topLeft);
+        frustum.SetRow(1, topRight);
+        frustum.SetRow(2, bottomRight);
+        frustum.SetRow(3, bottomLeft);
+
+
+        return frustum;
+    }
+}
